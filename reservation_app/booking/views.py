3a1972154b5fc3571125bfb4966112ee2c39d91d
Vlasov Alex <Vlas442004@gmail.com>
2025-05-09 00:54:29 +0500

    first commit

---
+++ reservation_app/booking/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        'products': ['Товар 1', 'Товар 2', 'Товар 3'],
+    }
+    return render(request, 'index.html', context)
